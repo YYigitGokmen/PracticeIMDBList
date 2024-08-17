39713970add719af766b5fb67a0bf4bb528e9f76
Yılmaz Yiğit Gökmen <gokmenyyigit@gmail.com>
2024-08-17 20:45:13 +0300

    Add files via upload

---
+++ Movie.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PracticeImdbList
+{
+    public class Movie
+    {
+            public double ımdbScore { get; set; }
+            public string name { get; set; }
+
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,85 @@
+﻿
+using PracticeImdbList;
+using System.Globalization;
+using System.Security.Cryptography.X509Certificates;
+using System.Xml.Linq;
+using static System.Formats.Asn1.AsnWriter;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+
+        List<Movie> Movies = new List<Movie>();
+
+
+        while (true)
+        {
+            Console.Write("Lüften film adı giriniz (Listeyi yazdırmak veya çıkmak için çıkış yazınız) : ");
+            string movieName = Console.ReadLine();
+
+
+            if (movieName.ToLower() == "çıkış")
+            {
+                break;
+            }
+
+            Console.Write("Lüften IMDB puanını giriniz :");
+            double ımdbScore;
+
+            while (!double.TryParse(Console.ReadLine(), NumberStyles.Any, CultureInfo.InvariantCulture, out ımdbScore) || ımdbScore < 0 || ımdbScore > 10)
+            {
+                Console.WriteLine("Lütfen 10 ve 0 arasında bir puan giriniz!");
+            }
+
+
+            Movie Movie = new Movie { name = movieName, ımdbScore = ımdbScore };
+            Movies.Add(Movie);
+
+            Console.WriteLine("Film Eklendi! \n");
+
+        }
+
+
+        Console.WriteLine("Girdiğiniz filmlerin listesi: \n");
+
+        foreach (Movie movie in Movies)
+        {
+            Console.WriteLine($"Film adı: {movie.name}, IMDB Puanı: {movie.ımdbScore}");
+        }
+
+
+        Console.WriteLine("IMDB Puanı 4 ve 9 arasında olan bütün filmler : \n");
+
+        var filteredMovies = Movies.Where(m => m.ımdbScore >= 4 && m.ımdbScore <= 9).ToList();
+
+        foreach (Movie movie in filteredMovies)
+        {
+            Console.WriteLine($"Film adı : {movie.name}, Imdb puanı : {movie.ımdbScore}");
+        }
+
+
+        Console.WriteLine("İsmi A harfi ile başlayan filmler : \n");
+
+        var moviesStartWithA = Movies.Where(Movie => Movie.name.StartsWith("A")).ToList();
+
+        foreach (Movie movie in moviesStartWithA)
+        {
+            Console.WriteLine($"{movie.name}, IMDB puanı: {movie.ımdbScore}");
+
+        }
+
+    }
+
+}
+
+
+
+
+
+
+
+
+
+
+
